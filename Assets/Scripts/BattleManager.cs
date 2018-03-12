060e66d83ae22f818cc00c00a3e5bbd6572aef1f
yangkai <yangkai92@gmail.com>
2018-03-12 18:28:21 +0800

    Setting the battle scene with Mecanim
    This is my 10th submit:
    The BattleManager was edited and advaned with BattleStateMachine by Mecanim in BattleScene.
    The introductory animation was done and complied with code in BattleManager.
    And HUD is to be finished.

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class BattleManager : MonoBehaviour {
+public class BattleManager : MonoBehaviour
+{
+    public CanvasGroup theButtons;
 
     // 配置战斗场景
     public GameObject[] EnemySpawnPoints;
@@ -12,27 +13,50 @@ public class BattleManager : MonoBehaviour {
 
     // 管理战斗场景的变量
     private int enemyCount;
-    enum BattlePhase
+    public enum BattleState
     {
-        PlayerAttack, EnemyAttack
+        Begin_Battle,
+        Intro,
+        Player_Move,
+        Player_Attack,
+        Change_Control,
+        Enemy_Attack,
+        Battle_Result,
+        Battle_End
     }
-    private BattlePhase phase;
 
-    public CanvasGroup theButtons;
+    private Dictionary<int, BattleState> battleStateHash = new Dictionary<int, BattleState>();
+    private BattleState currentBattleState;
 
-    // Use this for initialization
-    void Start () {
+    private Animator battleStateManager;
+
+    public GameObject introPanel;
+    Animator introPanelAnim;
+
+    void Awake()
+    {
+        introPanelAnim = introPanel.GetComponent<Animator>();
+
+        battleStateManager = (Animator)GetComponent(typeof(Animator));
+        if (battleStateManager == null)
+        {
+            Debug.LogError("No battleStateMachine Animator found.");
+        }
+    }
+
+    void Start ()
+    {
         // 初始化战斗场景并用协程生成随机数量的敌人
         enemyCount = Random.Range(1, EnemySpawnPoints.Length);
         StartCoroutine(SpawnEnemies());
-        phase = BattlePhase.PlayerAttack; 
+
+        GetAnimationStates();
     }
 
-    // Update is called once per frame
     void Update () {
         // 根据是否轮到玩家开启和关闭按钮
-        if (phase == BattlePhase.PlayerAttack)
-        {
+        if (currentBattleState == BattleState.Player_Move)
+            {
             theButtons.alpha = 1;
             theButtons.interactable = true;
             theButtons.blocksRaycasts = true;
@@ -42,6 +66,29 @@ public class BattleManager : MonoBehaviour {
             theButtons.alpha = 0;
             theButtons.interactable = false;
             theButtons.blocksRaycasts = false;
+        }
+
+        // 从状态机中获取当前状态，根据当前状态切换条件语句
+        currentBattleState = battleStateHash[battleStateManager.GetCurrentAnimatorStateInfo(0).shortNameHash];
+        switch (currentBattleState)
+        {
+            case BattleState.Intro:
+                introPanelAnim.SetTrigger("Intro");
+                break;
+            case BattleState.Player_Move:
+                break;
+            case BattleState.Player_Attack:
+                break;
+            case BattleState.Change_Control:
+                break;
+            case BattleState.Enemy_Attack:
+                break;
+            case BattleState.Battle_Result:
+                break;
+            case BattleState.Battle_End:
+                break;
+            default:
+                break;
         }
     }
 
@@ -55,6 +102,7 @@ public class BattleManager : MonoBehaviour {
             yield return StartCoroutine(MoveCharacterToPoint(EnemySpawnPoints[i], newEnemy));
             newEnemy.transform.parent =EnemySpawnPoints[i].transform;
         }
+        battleStateManager.SetBool("BattleReady", true);
     }
 
     // 将敌人从屏幕外移动到其预设的生成点
@@ -85,6 +133,15 @@ public class BattleManager : MonoBehaviour {
     {
         GameState.justExitedBattle = true;
         NavigationManager.NavigateTo("Overworld");
+    }
+
+    // 在Mecanim中为相应的动画状态生成一个散列，并将结果散列存储在字典中
+    void GetAnimationStates()
+    {
+        foreach (BattleState state in (BattleState[])System.Enum.GetValues(typeof(BattleState)))
+        {
+            battleStateHash.Add(Animator.StringToHash(state.ToString()), state);
+        }
     }
 
 }
