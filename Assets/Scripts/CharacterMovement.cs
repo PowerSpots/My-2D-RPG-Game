4d7e639df29caca7e414d740bec5bc536e52ea3c
Unknown <yangkai92@gmail.com>
2018-05-24 22:00:54 +0800

    This is my 16th submit. Some background items like mailbox, trees and wheels are added to the town view. And the character movement script is edit.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+/// <summary>
+/// 角色的移动控制
+/// </summary>
 public class CharacterMovement : MonoBehaviour
 {
-    // 玩家角色的刚体2D组件引用
+    // 玩家的RigidBody2D组件实例
     private Rigidbody2D playerRigidBody2D;
 
     // 玩家角色的动画组件引用
     private Animator playerAnim;
 
     // 玩家角色的精灵渲染器组件引用
-    private SpriteRenderer playerSpriteImage; 
+    private SpriteRenderer playerSpriteImage;
 
-    // 用于记录玩家角色移动的水平、垂直移动变量和速度变量
+    // 跟踪从输入获得的运动变量来控制运动方向
     private float movePlayerHorizontal;
     private float movePlayerVertical;
     private Vector2 movement;
 
-    // 玩家移动的速度修正
+    // 玩家移动的速度
     public float speed = 4.0f;
 
-    // 初始化组件引用
+    // 初始化所有组件
     void Awake()
     {
-        // 手动指明要获得的组件类型，加速组件的获得
+        // 明确所要求的组件类型
         playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
         playerAnim = (Animator)GetComponent(typeof(Animator));
         playerSpriteImage = (SpriteRenderer)GetComponent(typeof(SpriteRenderer));
@@ -30,21 +33,24 @@ public class CharacterMovement : MonoBehaviour
 
     void Update ()
     {
-        // 将输入所得的水平、垂直分量赋值给玩家的速度向量
+        // 使用默认的水平键（左和右）和垂直键（上和下）来检查玩家是否正在控制游戏
         movePlayerHorizontal = Input.GetAxis("Horizontal");
         movePlayerVertical = Input.GetAxis("Vertical");
+        // 施加有方向的力量来移动英雄
         movement = new Vector2(movePlayerHorizontal,movePlayerVertical);
-        playerRigidBody2D.velocity=movement*speed;
+        playerRigidBody2D.velocity = movement * speed;
 
-        // 角色移动的动画控制
+        // 角色行走的动画控制
         if (movePlayerVertical != 0)
         {
             playerAnim.SetBool("xMove", false);
             playerSpriteImage.flipX = false;
+            // 向上走
             if (movePlayerVertical > 0)
             {
                 playerAnim.SetInteger("yMove", 1);
             }
+            // 向下走
             else if (movePlayerVertical < 0)
             {
                 playerAnim.SetInteger("yMove", -1);
@@ -52,12 +58,15 @@ public class CharacterMovement : MonoBehaviour
         }
         else
         {
+            // 水平行走
             playerAnim.SetInteger("yMove", 0);
+            // 向右水平行走
             if (movePlayerHorizontal > 0)
             {
                 playerAnim.SetBool("xMove", true);
                 playerSpriteImage.flipX = false;
             }
+            // 向左水平行走
             else if (movePlayerHorizontal < 0)
             {
                 playerAnim.SetBool("xMove", true);
--- Assets/Scripts/Classes/Entity.cs
+++ Assets/Scripts/Classes/Entity.cs
@@ -1,26 +1,45 @@
 using UnityEngine;
+/// <summary>
+/// 所有角色的基类
+/// </summary>
 public class Entity : ScriptableObject
 {
+    // 姓名
     public string name;
+    // 年龄
     public int age;
+    // 帮派
     string faction;
+    // 职业
     public string occupation;
+    // 等级
     public int level = 1;
+    // 健康
     public int health = 2;
+    // 力量
     public int strength = 1;
+    // 魔法
     public int magic = 0;
+    // 防御
     public int defense = 0;
+    // 速度
     public int speed = 1;
+    // 伤害
     public int damage = 1;
+    // 护甲
     public int armor = 0;
+    // 攻击次数
     public int noOfAttacks = 1;
+    // 武器
     public string weapon;
+    // 位置
     public Vector2 position;
-
+    // 承受伤害时调用的方法
     public void TakeDamage(int Amount)
     {
         health = health - Mathf.Clamp((Amount - armor), 0, int.MaxValue);
     }
+    // 攻击时调用的方法
     public void Attack(Entity Entity)
     {
         Entity.TakeDamage(strength);
